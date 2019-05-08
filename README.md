7bc802d0e9d38e19896b96074965d6e387450fda
Yandy Ramirez <yandyr@gmail.com>
2019-05-08 10:03:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## AWS Log Group Deleter
+
+Simple `go` script to cleanup `cloudwatch` log groups for dev environments.
---
+++ main.go
@@ -0,0 +1,73 @@
+// Copyright 2019 Yandy Ramirez
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+)
+
+func main() {
+
+	// Set background context, concurrency safety
+	ctx := context.Background()
+
+	// Load AWS client config
+	// aws_key && aws_secret amongst others
+	// set the region
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		log.Fatalf("unable to load SDK config %v", err)
+	}
+	cfg.Region = endpoints.UsEast2RegionID
+
+	// Create cloudwatch client
+	cw := cloudwatchlogs.New(cfg)
+
+	// parameters for filtering the log groups
+	limit := aws.Int64(50)
+	params := cloudwatchlogs.DescribeLogGroupsInput{
+		Limit: limit,
+	}
+
+	// Create a request for the log groups within a region
+	a := cw.DescribeLogGroupsRequest(&params)
+	resp, err := a.Send(ctx)
+	if err != nil {
+		log.Fatalf("unable to request logs %v", err)
+	}
+
+	if len(resp.LogGroups) > 0 {
+		for _, v := range resp.LogGroups {
+			d := &cloudwatchlogs.DeleteLogGroupInput{
+				LogGroupName: v.LogGroupName,
+			}
+			del := cw.DeleteLogGroupRequest(d)
+			o, err := del.Send(ctx)
+			if err != nil {
+				log.Fatalf("could not delete log group %v", err)
+			}
+			fmt.Printf("Deleted log group %v", o.GoString())
+		}
+	}
+
+	fmt.Println("no log groups to delete")
+}
